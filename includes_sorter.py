b8c5465f0456249f53a31c96b4e996a318d82312
Andrey Matveyakin <a.matveyakin@gmail.com>
2014-03-15 19:18:17 +0400

    Add rule priorities; reduce verbosity

--- includes_sorter.py
+++ includes_sorter.py
@@ -23,8 +23,9 @@ class GroupProperties:
   NORMAL = 0
   EMPTY = 1
   MY_HEADER = 2
-  def __init__ (self, groupType, name, regex, sort, keepVSpace, keepDup):
+  def __init__ (self, groupType, priority, name, regex, sort, keepVSpace, keepDup):
     self.groupType = groupType
+    self.priority = priority
     self.name = name
     self.regex = re.compile (regex)
     self.sort = sort
@@ -46,31 +47,32 @@ class GroupProperties:
       return line.isspace ()
     elif self.groupType == self.MY_HEADER:
       return re.match (r' *# *include *".*\b{0}\.h"'.format (os.path.splitext (filename)[0]), line)
-  def greedyMatch (self):
-    return self.groupType == self.MY_HEADER
 
-groupProperties = [GroupProperties (GroupProperties.EMPTY     , 'Empty'   , r''                          , False, False, False),
-                   GroupProperties (GroupProperties.MY_HEADER , 'MyHeader', r''                          , False, False, False),
-                   GroupProperties (GroupProperties.NORMAL    , 'CStd'    , r' *# *include *<.+\.h>'     , True , False, False),
-                   GroupProperties (GroupProperties.NORMAL    , 'C++Std'  , r' *# *include *<[a-z_0-9]+>', True , False, False),
-                   GroupProperties (GroupProperties.NORMAL    , 'Qt'      , r' *# *include *<Q.+>'       , True , False, False),
-                   GroupProperties (GroupProperties.NORMAL    , 'Local'   , r' *# *include *".*"'        , False, True , False)]
+groupProperties = [GroupProperties (GroupProperties.EMPTY    , 4, 'Empty'   , r''                          , False, False, False),
+                   GroupProperties (GroupProperties.MY_HEADER, 3, 'MyHeader', r''                          , False, False, False),
+                   GroupProperties (GroupProperties.NORMAL   , 2, 'CStd'    , r' *# *include *<.+\.h>'     , True , False, False),
+                   GroupProperties (GroupProperties.NORMAL   , 2, 'C++Std'  , r' *# *include *<[a-z_0-9]+>', True , False, False),
+                   GroupProperties (GroupProperties.NORMAL   , 2, 'Qt'      , r' *# *include *<Q.+>'       , True , False, False),
+                   GroupProperties (GroupProperties.NORMAL   , 1, 'OtherLib', r' *# *include *<.+>'        , False, True , False),
+                   GroupProperties (GroupProperties.NORMAL   , 2, 'Local'   , r' *# *include *".+"'        , False, True , False)]
 
 for iGroup, props in enumerate (groupProperties):
   props.setId (iGroup)
 
+groupPropertiesByPriority = sorted (groupProperties, key = lambda props: -props.priority)
+
 anyIncludeRegex = re.compile (r' *# *include')
 
 def classifyLine (line, filename):
-  matchingGroup = None
-  for props in groupProperties:
+  matchingProps = None
+  for props in groupPropertiesByPriority:
+    if matchingProps != None and props.priority < matchingProps.priority:
+      return matchingProps
     if props.matches (line, filename):
-      if props.greedyMatch ():
-        return props
-      if matchingGroup != None:
-        raise RuntimeError ('Line \n' + line + ' matches both "' + matchingGroup.name + '" and "' + props.name + '" rules')
-      matchingGroup = props
-  return matchingGroup
+      if matchingProps != None:
+        raise RuntimeError ('  Line \n  {0}  matches both "{1}" and "{2}" rules'.format (line, matchingProps.name, props.name))
+      matchingProps = props
+  return matchingProps
 
 class Group:
   def __init__ (self, props):
@@ -114,11 +116,12 @@ class Chunk:
 for root, dirs, files in os.walk (srcRoot):
   files = [f for f in files if not f.startswith ('.')]
   dirs[:] = [d for d in dirs if not d.startswith ('.')]
+  print ('Entering folder "{0}"'.format (root))
+  # TODO: Also print `Leaving folder' message with stas ('{0} files found, {1} changed, {2} were lock for writing'); do not write locked for writing messages
   for relname in files:
     if not re.match (filenameMask, relname):
       continue
     filename = os.path.join (root, relname)
-    print ('Processing ' + filename + '...', end='')
     try:
       f = open (filename, 'r')
       oldContent = f.readlines ()
@@ -128,7 +131,7 @@ for root, dirs, files in os.walk (srcRoot):
         lineGroup = classifyLine (line, relname)
         isMatch = (lineGroup != None)
         if lineGroup == None and re.match (anyIncludeRegex, line):
-          raise RuntimeError ('Line \n' + line + ' contains an #include, but doesn\'t match any known include group')
+          raise RuntimeError ('  Line \n  {0}  contains an #include, but doesn\'t match any known include group'.format (line))
         if isMatch and (chunk or not lineGroup.isEmpty ()):
           if not chunk:
             chunk = Chunk ()
@@ -143,10 +146,9 @@ for root, dirs, files in os.walk (srcRoot):
         f = open (filename, 'w')
         f.writelines (newContent)
         f.close ()
-        print (' Done.')
       except:
-        print (' Locked.')
+        print (' File "{0}" is locked for writing'.format (filename))
     except RuntimeError as err:
-      print (' ERROR: {0}'.format (err))
+      print (' ERROR in file "{0}":\n{1}'.format (filename, err))
     except:
-      print (' Unknown ERROR')
+      print (' Unknown ERROR in file "{0}"'.format (filename))
